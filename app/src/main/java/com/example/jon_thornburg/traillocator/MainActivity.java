ee99c9ae9d977eb6402dcf9b75ad20bdfdfcd3bd
Jon Thornburg <jon_thornburg@clickherelabs.com>
2017-02-10 17:28:44 -0600

    WIP

--- app/src/main/java/com/example/jon_thornburg/traillocator/MainActivity.java
+++ app/src/main/java/com/example/jon_thornburg/traillocator/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.jon_thornburg.traillocator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+
+import com.example.jon_thornburg.traillocator.Fragments.InitialFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String title = getResources().getString(R.string.app_name);
+        SpannableString s = new SpannableString(title);
+        s.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorDarkBlue)), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getSupportActionBar().setTitle(s);
+
+        if (savedInstanceState == null) {
+            Fragment frag = InitialFragment.createNew();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.main_content, frag, InitialFragment.TAG);
+            ft.commit();
+        }
     }
+
+
 }
